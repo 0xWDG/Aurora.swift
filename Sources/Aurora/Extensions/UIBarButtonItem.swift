3311b3890a029f691c163878500a4f93d59b92da
Wesley de Groot <wes@vista.aero>
2020-04-01 17:31:02 +0200

    Public, Public, and Public

--- Sources/Aurora/Extensions/UIBarButtonItem.swift
+++ Sources/Aurora/Extensions/UIBarButtonItem.swift
@@ -4,14 +4,14 @@ import Foundation
 import UIKit
 
 extension UIBarButtonItem {
-    var frame: CGRect? {
+    public var frame: CGRect? {
         guard let view = self.value(forKey: "view") as? UIView else {
             return nil
         }
         return view.frame
     }
     
-    var view: UIView? {
+    public var view: UIView? {
         guard let view = self.value(forKey: "view") as? UIView else {
             return nil
         }
