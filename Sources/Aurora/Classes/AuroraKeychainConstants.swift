6b5186b4ce008d4024e97af90c9667a5ee82714e
Wesley de Groot <wes@vista.aero>
2021-02-26 14:42:48 +0100

    Fix linting errors

---
+++ Sources/Aurora/Classes/AuroraKeychainConstants.swift
@@ -0,0 +1,84 @@
+// AURORA Keychain.
+
+#if canImport(Security)
+import Foundation
+import Security
+
+/// These options are used to determine when a keychain item should be readable.
+/// The default value is AccessibleWhenUnlocked.
+public enum KeychainSwiftAccessOptions {
+    case accessibleWhenUnlocked
+    case accessibleWhenUnlockedThisDeviceOnly
+    case accessibleAfterFirstUnlock
+    case accessibleAfterFirstUnlockThisDeviceOnly
+    case accessibleWhenPasscodeSetThisDeviceOnly
+    
+    static var defaultOption: KeychainSwiftAccessOptions {
+        return .accessibleWhenUnlocked
+    }
+    
+    var value: String {
+        switch self {
+        case .accessibleWhenUnlocked:
+            return toString(kSecAttrAccessibleWhenUnlocked)
+            
+        case .accessibleWhenUnlockedThisDeviceOnly:
+            return toString(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
+            
+        case .accessibleAfterFirstUnlock:
+            return toString(kSecAttrAccessibleAfterFirstUnlock)
+            
+        case .accessibleAfterFirstUnlockThisDeviceOnly:
+            return toString(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly)
+            
+        case .accessibleWhenPasscodeSetThisDeviceOnly:
+            return toString(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly)
+        }
+    }
+    
+    func toString(_ value: CFString) -> String {
+        return KeychainSwiftConstants.toString(value)
+    }
+}
+
+/// Constants used by the library
+public struct KeychainSwiftConstants {
+    /// Specifies a Keychain access group. Used for sharing Keychain items between apps.
+    public static var accessGroup: String { return toString(kSecAttrAccessGroup) }
+    
+    /// A value that indicates when your app needs access to the data in a keychain item.
+    /// The default value is AccessibleWhenUnlocked. For a list of possible values, see KeychainSwiftAccessOptions.
+    public static var accessible: String { return toString(kSecAttrAccessible) }
+    
+    /// Used for specifying a String key when setting/getting a Keychain value.
+    public static var attrAccount: String { return toString(kSecAttrAccount) }
+    
+    /// Used for specifying synchronization of keychain items between devices.
+    public static var attrSynchronizable: String { return toString(kSecAttrSynchronizable) }
+    
+    /// An item class key used to construct a Keychain search dictionary.
+    public static var klass: String { return toString(kSecClass) }
+    
+    /// Specifies the number of values returned from the keychain. The library only supports single values.
+    public static var matchLimit: String { return toString(kSecMatchLimit) }
+    
+    /// A return data type used to get the data from the Keychain.
+    public static var returnData: String { return toString(kSecReturnData) }
+    
+    /// Used for specifying a value when setting a Keychain value.
+    public static var valueData: String { return toString(kSecValueData) }
+    
+    /// Used for returning a reference to the data from the keychain
+    public static var returnReference: String { return toString(kSecReturnPersistentRef) }
+    
+    /// A key whose value is a Boolean indicating whether or not to return item attributes
+    public static var returnAttributes: String { return toString(kSecReturnAttributes) }
+    
+    /// A value that corresponds to matching an unlimited number of items
+    public static var secMatchLimitAll: String { return toString(kSecMatchLimitAll) }
+    
+    static func toString(_ value: CFString) -> String {
+        return value as String
+    }
+}
+#endif
