419b67126c3d2b4256f320cc9224328128e75353
Wesley de Groot <wes@vista.aero>
2021-07-16 17:23:38 +0200

    Added docs for Threads

--- Sources/Aurora/Aurora Extensions/Aurora.Threads.swift
+++ Sources/Aurora/Aurora Extensions/Aurora.Threads.swift
@@ -17,79 +17,35 @@
 
 import Foundation
 
-infix operator ~>   // serial queue operator
-
-/// Executes the lefthand closure on a background thread and,
-/// upon completion, the righthand closure on the main thread.
-/// Passes the background closure's output to the main closure.
-/// - Parameters:
-///   - backgroundClosure: <#backgroundClosure description#>
-///   - mainClosure: <#mainClosure description#>
-public func ~> (backgroundClosure: @escaping () -> Void, mainClosure: @escaping () -> Void) {
-    serialQueue.async {
-        backgroundClosure()
-        DispatchQueue.main.async {
-            mainClosure()
-        }
-    }
-}
-/// <#Description#>
-/// - Parameters:
-///   - backgroundClosure: <#backgroundClosure description#>
-///   - mainClosure: <#mainClosure description#>
-public func ~> <R> (backgroundClosure: @escaping () -> R, mainClosure: @escaping (_ result: R) -> Void) {
-    serialQueue.async {
-        let result = backgroundClosure()
-        DispatchQueue.main.async(execute: {
-            mainClosure(result)
-        })
-    }
-}
-/// <#Description#>
-/// - Parameters:
-///   - backgroundClosure: <#backgroundClosure description#>
-///   - mainClosure: <#mainClosure description#>
-public func ~> (backgroundClosure: @escaping () -> String, mainClosure: @escaping (_ result: String) -> Void) {
-    serialQueue.async {
-        let result = backgroundClosure()
-        DispatchQueue.main.async(execute: {
-            mainClosure(result)
-        })
-    }
-}
-
-/// Serial dispatch queue used by the ~> operator.
-private let serialQueue = DispatchQueue(label: "serial-worker")
-
 public extension Aurora {
-    /// <#Description#>
-    /// - Parameter block: <#block description#>
+    /// run in background
+    /// - Parameter block: what to run
     func runInBackground(block: @escaping () -> Void) {
         DispatchQueue.global(qos: .background).async {
             block()
         }
     }
     
-    /// <#Description#>
-    /// - Parameter block: <#block description#>
+    /// run on main thread (foreground)
+    /// - Parameter block: what to run
     func runInForeground(block: @escaping () -> Void) {
         DispatchQueue.main.async {
             block()
         }
     }
     
-    /// repeat/run
+    /// Run every
     /// - Parameters:
-    ///   - every: <#every description#>
-    ///   - block: <#block description#>
+    ///   - every: time interval
+    ///   - block: what to run
     func run(every: TimeInterval, block: @escaping (Timer) -> Void) {
         Timer.scheduledTimer(withTimeInterval: every, repeats: true, block: block)
     }
     
-    /// <#Description#>
+    /// Run
     /// - Parameters:
-    ///   - background: <#background description#>
-    ///   - foreground: <#foreground description#>
+    ///   - background: what to run in background
+    ///   - foreground: what to run in foreground
     func run(background: @escaping () -> String,
              foreground: @escaping (_ returning: String) -> Void) {
         DispatchQueue.global(qos: .background).async {
@@ -101,10 +57,10 @@ public extension Aurora {
         }
     }
     
-    /// <#Description#>
+    /// Run
     /// - Parameters:
-    ///   - background: <#background description#>
-    ///   - foreground: <#foreground description#>
+    ///   - background: what to run in background
+    ///   - foreground: what to run in foreground
     func run(background: @escaping () -> Bool,
              foreground: @escaping (_ returning: Bool) -> Void) {
         DispatchQueue.global(qos: .background).async {
@@ -117,10 +73,10 @@ public extension Aurora {
         }
     }
     
-    /// <#Description#>
+    /// Run
     /// - Parameters:
-    ///   - background: <#background description#>
-    ///   - foreground: <#foreground description#>
+    ///   - background: what to run in background
+    ///   - foreground: what to run in foreground
     func run(background: @escaping () -> Any,
              foreground: @escaping (_ returning: Any) -> Void) {
         DispatchQueue.global(qos: .background).async {
