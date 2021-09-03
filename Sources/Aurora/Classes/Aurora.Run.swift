3f96df5b3c41d6e50fbe93b96737495f8b65304e
Wesley de Groot <wes@vista.aero>
2021-09-03 15:23:40 +0200

    Upgrade classes

---
+++ Sources/Aurora/Classes/Aurora.Run.swift
@@ -0,0 +1,47 @@
+// Aurora framework for Swift
+//
+// The **Aurora.framework** contains a base for your project.
+//
+// It has a lot of extensions built-in to make development easier.
+//
+// - Version: 1.0
+// - Copyright: [Wesley de Groot](https://wesleydegroot.nl) ([WDGWV](https://wdgwv.com))\
+//  and [Contributors](https://github.com/AuroraFramework/Aurora.swift/graphs/contributors).
+//
+// Thanks for using!
+//
+// Licence: MIT
+
+import Foundation
+
+extension Aurora {
+    /// Run actions after another
+    public class Run {
+        /// Alias for closire
+        typealias Action = () -> Void
+
+        /// The queue
+        var queue: [Action] = [Action]()
+
+        /// Initialize the first action
+        /// - Parameter act: Action closure
+        init(action: @escaping Action) {
+            queue.append(action)
+        }
+
+        /// Append execution in the queue
+        /// - Parameter action: Action closure
+        /// - Returns: Self (for appending)
+        func then(action: @escaping Action) -> Self {
+            queue.append(action)
+            return self
+        }
+
+        /// Run all the queue items.
+        deinit {
+            for item in queue {
+                item()
+            }
+        }
+    }
+}
---
+++ Sources/Aurora/Classes/Aurora.Validator.swift
@@ -0,0 +1,101 @@
+// Aurora framework for Swift
+//
+// The **Aurora.framework** contains a base for your project.
+//
+// It has a lot of extensions built-in to make development easier.
+//
+// - Version: 1.0
+// - Copyright: [Wesley de Groot](https://wesleydegroot.nl) ([WDGWV](https://wdgwv.com))\
+//  and [Contributors](https://github.com/AuroraFramework/Aurora.swift/graphs/contributors).
+//
+// Thanks for using!
+//
+// Licence: MIT
+
+import Foundation
+
+extension Aurora {
+    /// Validate input
+    open class Validator {
+        /// Shared instance
+        public static let shared = Validator.init()
+
+        /// initialize
+        public init() {}
+
+        /// Contains banned words?
+        /// - Parameter str: String to be checked.
+        /// - Returns: Contains banned words?
+        public func containsBannedWord(str: String) -> Bool {
+            let bannedWords = [
+                "neuken",
+                "fuck",
+                "lul",
+                "hoer",
+                "i hate this app"
+            ]
+
+            return bannedWords.filter {
+                str.contains($0)
+            }.count >= 1
+        }
+
+        /// Does the string contains a phone number
+        /// - Parameter str: String to be checked.
+        /// - Returns: Contains phone number?
+        public func containsPhoneNumber(str: String) -> Bool {
+            do {
+                let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+                let matches = detector?.matches(
+                    in: str,
+                    options: [],
+                    range: NSRange(str.startIndex..., in: str)
+                )
+
+                if let res = matches?.first {
+                    return (res.resultType == .phoneNumber && res.range.length > 0)
+                } else {
+                    return false
+                }
+            }
+        }
+
+        /// Does the string contains a email adress
+        /// - Parameter str: String to be checked.
+        /// - Returns: Contains email adress?
+        public func containsEmailaddress(str: String) -> Bool {
+            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+            do {
+                let regex = try? NSRegularExpression(pattern: emailRegEx, options: [])
+                let nsString = NSString(string: str)
+                let results = regex?.matches(
+                    in: str,
+                    options: [],
+                    range: NSRange(location: 0, length: nsString.length)
+                )
+
+                return (results?.count ?? 0) > 0
+            }
+        }
+
+        /// Does the string contains a adress
+        /// - Parameter str: String to be checked.
+        /// - Returns: Contains adress?
+        public func containsAddress(str: String) -> Bool {
+            do {
+                let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.address.rawValue)
+                let matches = detector?.matches(
+                    in: str,
+                    options: [],
+                    range: NSRange(str.startIndex..., in: str)
+                )
+
+                if let res = matches?.first {
+                    return (res.resultType == .address && res.range.length > 0)
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+}
