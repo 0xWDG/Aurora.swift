b854bda461a6b96b6c0a29cf38f43a28074809f5
Wesley de Groot <wes@vista.aero>
2022-02-04 16:48:26 +0100

    Added some SwiftUI Classes

---
+++ Sources/Aurora/SwiftUI/SwiftUI.AttributedText.swift
@@ -0,0 +1,67 @@
+// Aurora framework for Swift
+//
+// The **Aurora.framework** contains a base for your project.
+//
+// It has a lot of extensions built-in to make development easier.
+//
+// - Version: 1.0
+// - Copyright: [Wesley de Groot](https://wesleydegroot.nl) ([WDGWV](https://wdgwv.com))\
+//  and [Contributors](https://github.com/AuroraFramework/Aurora.swift/graphs/contributors).
+//
+// Thanks for using!
+//
+// Licence: MIT
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+struct AttributedText: View {
+    @State private var size: CGSize = .zero
+    let attributedString: NSAttributedString
+
+    init(_ attributedString: NSAttributedString) {
+        self.attributedString = attributedString
+    }
+
+    init(HTML: String) {
+        if let HTMLData = HTML.data(using: .utf8),
+           let attributedString = try? NSAttributedString(
+            data: HTMLData,
+            options: [.documentType: NSAttributedString.DocumentType.html],
+            documentAttributes: nil
+           ) {
+            self.init(attributedString)
+            return
+        }
+
+        self.init(.init(string: "Failed to decode HTML"))
+    }
+
+    var body: some View {
+        AttributedTextRepresentable(attributedString: attributedString, size: $size)
+            .frame(width: size.width, height: size.height)
+    }
+
+    struct AttributedTextRepresentable: UIViewRepresentable {
+        let attributedString: NSAttributedString
+        @Binding var size: CGSize
+
+        func makeUIView(context: Context) -> UILabel {
+            let label = UILabel()
+
+            label.lineBreakMode = .byClipping
+            label.numberOfLines = 0
+
+            return label
+        }
+
+        func updateUIView(_ uiView: UILabel, context: Context) {
+            uiView.attributedText = attributedString
+
+            DispatchQueue.main.async {
+                size = uiView.sizeThatFits(uiView.superview?.bounds.size ?? .zero)
+            }
+        }
+    }
+}
+#endif
---
+++ Sources/Aurora/SwiftUI/SwiftUI.SafariView.swift
@@ -0,0 +1,43 @@
+// Aurora framework for Swift
+//
+// The **Aurora.framework** contains a base for your project.
+//
+// It has a lot of extensions built-in to make development easier.
+//
+// - Version: 1.0
+// - Copyright: [Wesley de Groot](https://wesleydegroot.nl) ([WDGWV](https://wdgwv.com))\
+//  and [Contributors](https://github.com/AuroraFramework/Aurora.swift/graphs/contributors).
+//
+// Thanks for using!
+//
+// Licence: MIT
+
+#if canImport(swiftUI) && canImport(SafariServices)
+import SwiftUI
+import SafariServices
+import UIKit
+import Aurora
+
+/// Make a Safari View for SwiftUI
+struct SafariView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = SFSafariViewController
+
+    @Binding var urlString: String
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        guard let url = URL(string: urlString.latinized) else {
+            fatalError("Invalid urlString: \(urlString)")
+        }
+
+        let safariViewController = SFSafariViewController(url: url)
+        safariViewController.preferredControlTintColor = UIColor(Color.accentColor)
+        safariViewController.dismissButtonStyle = .close
+
+        return safariViewController
+    }
+
+    func updateUIViewController(_ safariViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        return
+    }
+}
+#endif
